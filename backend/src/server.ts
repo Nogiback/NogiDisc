92cb3b11be781287bf3e5e8d189d93db1b6593af
Peter Do <peter.toan.do@gmail.com>
2024-11-05 14:34:11 -0500

    Pushing schema changes to db, setting up controllers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,9 +8,7 @@ import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World.");
-// });
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/inventory", inventoryRoutes);
