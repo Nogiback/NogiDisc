0310cae68bffcc193dcb02a55369b8672be806c1
Peter Do <peter.toan.do@gmail.com>
2024-11-07 15:39:36 -0500

    Adding login functionality and testing signup

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -5,6 +5,8 @@ import prisma from "../db/prisma.js";
 import bcrypt from "bcryptjs";
 import generateToken from "../utils/generateToken.js";
 
+// SIGN UP FUNCTION
+
 export const signup = [
   body("email", "Email must not be empty.")
     .trim()
@@ -74,5 +76,58 @@ export const signup = [
   }),
 ];
 
-export const login = async (req: Request, res: Response) => {};
+// LOGIN FUNCTION
+
+export const login = [
+  body("username", "Username must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password must not be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req: Request, res: Response, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(401).json({
+        errors: errors.array(),
+        message: "Error: Login Failure.",
+      });
+      return;
+    }
+
+    const user = await prisma.user.findUnique({ where: req.body.username });
+
+    if (!user) {
+      return res.status(401).json({ message: "No user found." });
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isValidPassword) {
+      return res
+        .status(401)
+        .json({ message: "Incorrect username or password." });
+    }
+
+    generateToken(user.id, res);
+
+    res.status(200).json({
+      _id: user.id,
+      email: user.email,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      message: "User successfully logged in.",
+    });
+  }),
+];
+
+// LOGOUT FUNCTION
+
 export const logout = async (req: Request, res: Response) => {};
