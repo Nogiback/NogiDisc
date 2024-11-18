9e9a805192ae8c82b2212047823b79f8919554a3
Peter Do <peter.toan.do@gmail.com>
2024-11-18 17:38:06 -0500

    finishing editbag and editdisc functionality in bag and disc controllers

--- backend/src/controllers/bagController.ts
+++ backend/src/controllers/bagController.ts
@@ -3,6 +3,8 @@ import asyncHandler from "express-async-handler";
 import { body, validationResult } from "express-validator";
 import prisma from "../db/prisma.js";
 
+// GET UNIQUE BAG BY ID
+
 export const getBag = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const bagID = req.params.bagID;
@@ -18,6 +20,8 @@ export const getBag = asyncHandler(
   }
 );
 
+// CREATE NEW BAG
+
 export const createBag = [
   body("name", "Name cannot be empty.").trim().isLength({ min: 1 }),
 
@@ -32,11 +36,22 @@ export const createBag = [
       return;
     }
 
-    const name = req.body.name;
+    const bagName = req.body.name;
+
+    const isNameTaken = await prisma.bag.findUnique({
+      where: { name: bagName },
+    });
+
+    if (isNameTaken) {
+      res
+        .status(409)
+        .json({ message: "Error: A bag with that name already exists." });
+      return;
+    }
 
     const newBag = await prisma.bag.create({
       data: {
-        name,
+        name: bagName,
         userID: req.user.id,
       },
     });
@@ -45,15 +60,64 @@ export const createBag = [
   }),
 ];
 
-export const editBag = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+// EDIT BAG BY ID
+
+export const editBag = [
+  body("name").trim().optional(),
+
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(401).json({
+        errors: errors.array(),
+        message: "Error: Bag creation Failure.",
+      });
+      return;
+    }
+
     // check if bag exists
+    const bagID = req.params.bagID;
+
+    const checkBag = await prisma.bag.findUnique({
+      where: { id: bagID },
+    });
+
     // if not, return error
+    if (!checkBag) {
+      res.status(404).json({ message: "Error: No bag found." });
+      return;
+    }
+
     // if yes, sanitize/check form data
-    // if removing disc, also remove bag from disc entity
+    const newBagName = req.body.name ? req.body.name : checkBag.name;
+
+    const isNameTaken = await prisma.bag.findUnique({
+      where: { name: newBagName },
+    });
+
+    if (isNameTaken) {
+      res
+        .status(409)
+        .json({ message: "Error: A bag with that name already exists." });
+      return;
+    }
+
     // if no errors, update bag
-  }
-);
+    const updatedBag = await prisma.bag.update({
+      where: {
+        id: bagID,
+      },
+      data: {
+        name: newBagName,
+      },
+    });
+
+    res.status(200).json(updatedBag);
+  }),
+];
+
+// DELETE BAG BY ID
 
 export const deleteBag = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -64,7 +128,7 @@ export const deleteBag = asyncHandler(
 
     // if not, return error
     if (!bagToDelete) {
-      res.status(404).json({ message: "Error: Disc not found." });
+      res.status(404).json({ message: "Error: Bag not found." });
       return;
     }
 
--- backend/src/controllers/inventoryController.ts
+++ backend/src/controllers/inventoryController.ts
@@ -3,6 +3,8 @@ import asyncHandler from "express-async-handler";
 import { body, validationResult } from "express-validator";
 import prisma from "../db/prisma.js";
 
+// GET DISC BY ID
+
 export const getDisc = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
     const discID = req.params.discID;
@@ -17,6 +19,8 @@ export const getDisc = asyncHandler(
     res.status(200).json(disc);
   }
 );
+
+// CREATE NEW DISC
 
 export const createDisc = [
   body("manufacturer", "Manufacturer cannot be empty.")
@@ -78,15 +82,84 @@ export const createDisc = [
   }),
 ];
 
-export const editDisc = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+// UPDATE DISC BY ID
+
+export const editDisc = [
+  body("manufacturer", "Manufacturer cannot be empty.")
+    .trim()
+    .isLength({ min: 1 }),
+  body("name", "Name cannot be empty.").trim().isLength({ min: 1 }),
+  body("weight", "Weight cannot be empty.").notEmpty().isInt(),
+  body("category", "Category cannot be empty.").notEmpty().isString(),
+  body("colour").isString(),
+  body("speed", "Speed cannot be empty.")
+    .notEmpty()
+    .isFloat({ min: 1, max: 14.5 }),
+  body("glide", "Glide cannot be empty.")
+    .notEmpty()
+    .isFloat({ min: 1, max: 7 }),
+  body("turn", "Turn cannot be empty.").notEmpty().isFloat({ min: -7, max: 0 }),
+  body("fade", "Fade cannot be empty.").notEmpty().isFloat({ min: 0, max: 5 }),
+  body("bagID").isString().optional({ values: "falsy" }),
+
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(401).json({
+        errors: errors.array(),
+        message: "Error: Disc creation Failure.",
+      });
+      return;
+    }
+
     // check if disc exists
+    const discID = req.params.discID;
+    const checkDisc = await prisma.disc.findUnique({
+      where: { id: discID },
+    });
+
     // if not, return error
+    if (!checkDisc) {
+      res.status(404).json({ message: "Error: No disc found." });
+      return;
+    }
+
     // if yes, sanitize/check form data
-    // if removed from bag, also remove disc from bag entity
+    const manufacturer = req.body.manufacturer;
+    const name = req.body.name;
+    const weight = req.body.weight;
+    const category = req.body.category;
+    const colour = req.body.colour;
+    const speed = req.body.speed;
+    const glide = req.body.glide;
+    const turn = req.body.turn;
+    const fade = req.body.fade;
+    const bagID = req.body.bagID;
+
     // if no errors, update disc
-  }
-);
+    const updatedDisc = await prisma.disc.update({
+      where: {
+        id: discID,
+      },
+      data: {
+        manufacturer,
+        name,
+        weight,
+        category,
+        colour,
+        speed,
+        glide,
+        turn,
+        fade,
+        userID: req.user.id,
+        bagID: bagID ? bagID : null,
+      },
+    });
+
+    res.status(200).json(updatedDisc);
+  }),
+];
 
 export const deleteDisc = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
