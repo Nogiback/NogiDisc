8f65f22b2b60c2e8da2852d7e799779520827632
Peter Do <peter.toan.do@gmail.com>
2024-11-21 18:37:36 -0500

    Adding edit user profile functionality to userController

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -70,14 +70,64 @@ export const getAllUserBags = asyncHandler(
   }
 );
 
-export const updateUser = asyncHandler(
-  async (req: Request, res: Response, next: NextFunction) => {
+export const updateUser = [
+  body("email", "Email must not be empty.")
+    .trim()
+    .isEmail()
+    .isLength({ min: 1 })
+    .escape(),
+  body("firstName", "First Name must not be empty.").trim().escape(),
+  body("lastName", "Last Name must not be empty.").trim().escape(),
+
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     // lookup user
+    const currentUserID = req.user.id;
+    const currentUser = await prisma.user.findUnique({
+      where: { id: currentUserID },
+    });
+
     // if no user, send error
+    if (!currentUser) {
+      res.status(404).json({ message: "Error: No user found." });
+      return;
+    }
+
     // if user, sanitize/check form data
-    // update user and return 200
-  }
-);
+
+    const duplicateUser = await prisma.user.findUnique({
+      where: { email: req.body.email },
+    });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({
+        errors: errors.array(),
+        message: "Error: User update failure.",
+      });
+      return;
+    }
+
+    if (duplicateUser && duplicateUser.id !== currentUserID) {
+      res.status(409).json({
+        message: "Error: Email already exists.",
+      });
+      return;
+    }
+
+    // if not errors, update user and return 200
+    let updatedUser = await prisma.user.update({
+      where: { id: currentUserID },
+      data: {
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+    });
+
+    res.status(200).json(updatedUser);
+  }),
+];
 
 export const deleteUser = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
