4cfb9f85c61240a52f055e4b80d30faeaa1f55f3
Peter Do <peter.toan.do@gmail.com>
2025-02-03 12:53:51 -0500

    Fixing close button on dialogs to appear correctly in dark/light modes, starting disc Cards sections for dashboard

---
+++ frontend/src/components/dashboard/DiscsContainer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function DiscsContainer() {
+  return <div>DiscsContainer</div>;
+}
---
+++ frontend/src/components/disc/DiscCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function DiscCard() {
+  return <div>DiscCard</div>;
+}
