550123b7018626b9bbfdf0066c2f4d875af174d7
Peter Do <peter.toan.do@gmail.com>
2025-04-05 14:38:07 -0400

    Adding image ti disc card list version

--- frontend/src/components/cards/DiscCardList.tsx
+++ frontend/src/components/cards/DiscCardList.tsx
@@ -14,27 +14,36 @@ export default function DiscCardList({ disc }: DiscCardListProps) {
         <DeleteDiscModal discID={disc.id} />
       </div>
       <CardContent className='flex w-full items-center justify-start gap-6 p-4'>
-        <div
-          className='flex h-28 w-28 items-center justify-center rounded-full group-hover:animate-[spin_0.5s_linear_infinite]'
-          style={{ backgroundColor: disc.colour ? disc.colour : 'gray' }}
-        >
-          {brandLogo && (
-            <img
-              alt={`${brandLogo} brand logo`}
-              className='w-12 opacity-50'
-              src={brandLogo}
-            ></img>
-          )}
-          {!brandLogo && (
-            <p
-              aria-label={`${disc.brand} brand logo`}
-              className='bg-inherit bg-clip-text text-center font-bold text-transparent opacity-50'
-              style={{ filter: 'invert(1) grayscale(1) contrast(100)' }}
-            >
-              {disc.brand}
-            </p>
-          )}
-        </div>
+        {!disc.image && (
+          <div
+            className='flex h-28 w-28 items-center justify-center rounded-full group-hover:animate-[spin_0.5s_linear_infinite]'
+            style={{ backgroundColor: disc.colour ? disc.colour : 'gray' }}
+          >
+            {brandLogo && (
+              <img
+                alt={`${brandLogo} brand logo`}
+                className='w-12 opacity-50'
+                src={brandLogo}
+              ></img>
+            )}
+            {!brandLogo && (
+              <p
+                aria-label={`${disc.brand} brand logo`}
+                className='bg-inherit bg-clip-text text-center font-bold text-transparent opacity-50'
+                style={{ filter: 'invert(1) grayscale(1) contrast(100)' }}
+              >
+                {disc.brand}
+              </p>
+            )}
+          </div>
+        )}
+        {disc.image && (
+          <img
+            alt={`${disc.name} disc image`}
+            className='h-28 w-28 rounded-full object-cover group-hover:animate-[spin_0.5s_linear_infinite]'
+            src={disc.image}
+          ></img>
+        )}
         <div className='flex flex-col items-start justify-center'>
           <h3 className='text-2xl font-bold'>{disc.name}</h3>
           <span>
